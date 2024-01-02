2e3e188f9ab3c250fbbf7b6f78c7c1e1a11eda47
Yan Yu <bx6xbChosen1@gmail.com>
2024-01-02 23:19:23 +0600

    changed project structure, added webpack and changed some pictures format from png to webp

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const CopyPlugin = require("copy-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const path = require("path")
+
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development"
+
+  return {
+    mode: argv.mode,
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "js/index.[contenthash].js",
+      clean: true,
+    },
+    devServer: {
+      open: true,
+      port: 6900,
+      watchFiles: "src/**/*",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.sass$/,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        isDev
+          ? ""
+          : {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-env"],
+                },
+              },
+            },
+        {
+          test: /\.woff2$/,
+          type: "asset/resource",
+          generator: {
+            filename: "fonts/[name][ext]",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html",
+        inject: "body",
+        favicon: "./src/logo.png",
+      }),
+      isDev
+        ? ""
+        : new MiniCssExtractPlugin({
+            filename: "css/style.min.[contenthash].css",
+          }),
+      new CopyPlugin({
+        patterns: [{ from: "./src/imgs", to: "./imgs" }],
+      }),
+    ],
+  }
+}
